f1a9b17eacdd968a52191a7abb469c6cbe64546c
SadKoldun <serezhashostak9@gmail.com>
2023-08-26 16:34:56 +0300

    Async httpx requests v2

--- main.py
+++ main.py
@@ -5,39 +5,40 @@ from models import User_Pydantic, Users, Posts_Pydantic, Posts
 
 from tortoise.contrib.fastapi import register_tortoise
 
-app = FastAPI(title="Tortoise ORM FastAPI example")
+app = FastAPI(title="DataBase users and posts")
 
 
 @app.get("/upload-db")
 async def update_users():
     async with httpx.AsyncClient() as client:
-        response_users = await client.get(f"https://gorest.co.in/public/v2/users?page=1&per_page=100")
-        response_users = response_users.json()
-        for user in response_users:
-            new_user = Users(user_id=user["id"],
-                             name=user["name"],
-                             email=user["email"],
-                             gender=user["gender"],
-                             status=user["status"],
-                             )
-            try:
-                await new_user.save()
-            except tortoise.exceptions.IntegrityError:
-                continue
+        for page_number in range(1, 5):
+            response_users = await client.get(f"https://gorest.co.in/public/v2/users?page={page_number}&per_page=100")
+            response_users = response_users.json()
+            for user in response_users:
+                new_user = Users(user_id=user["id"],
+                                 name=user["name"],
+                                 email=user["email"],
+                                 gender=user["gender"],
+                                 status=user["status"],
+                                 )
+                try:
+                    await new_user.save()
+                except tortoise.exceptions.IntegrityError:
+                    continue
 
-    async with httpx.AsyncClient() as client:
-        response_posts = await client.get("https://gorest.co.in/public/v2/posts?page=1&per_page=100")
-        response_posts = response_posts.json()
-        for post in response_posts:
-            new_post = Posts(post_id=post["id"],
-                             title=post["title"],
-                             body=post["body"],
-                             user_id=post["user_id"],
-                             )
-            try:
-                await new_post.save()
-            except tortoise.exceptions.IntegrityError:
-                continue
+        for page_number in range(1, 10):
+            response_posts = await client.get(f"https://gorest.co.in/public/v2/posts?page={page_number}&per_page=100")
+            response_posts = response_posts.json()
+            for post in response_posts:
+                new_post = Posts(post_id=post["id"],
+                                 title=post["title"],
+                                 body=post["body"],
+                                 user_id=post["user_id"],
+                                 )
+                try:
+                    await new_post.save()
+                except tortoise.exceptions.IntegrityError:
+                    continue
 
     return {"message": "successfully updated"}
 
